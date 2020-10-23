d1cad103956b5d40e12e462462826fa79d24a9f1
Jasmine <jwang2367@gmail.com>
2020-10-22 21:31:52 -0400

    it wooooorks!

--- Demo.java
+++ Demo.java
@@ -8,11 +8,60 @@ public class Demo{
     }
   }
 
-  public static void main(String[] args) {
+  /* public static void main(String[] args) {
     if (args.length > 0) {
       printLoop(Integer.parseInt(args[0]));
     }
     else
       printLoop(5);
+    } */
+
+    public static String arrToString(int[] arr) {
+      String result = "{";
+      for (int i = 0; i < arr.length; i++) {
+        if (i < arr.length -1) {
+          result += Integer.toString(arr[i]) + ", ";
+        }
+        else result += Integer.toString(arr[i]);
+      }
+      result += "}";
+      return result;
     }
+
+    public static String arrayDeepToString(int[][]arr) {
+      String result = "{";
+
+      for (int i = 0; i < arr.length; i++) {
+        if (i < arr.length-1) {
+          result += arrToString(arr[i]) + ", ";
+        }
+        else result += arrToString(arr[i]);
+      result += "}";
+    }
+      return result;
+    }
+
+    public static int[][] create2DArray(int rows, int cols, int maxValue) {
+      int[][]myArray = new int[rows][cols];
+
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+          myArray[i][j] = (int)(Math.random()* maxValue);
+        }
+      }
+      return myArray;
+    }
+
+    public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+      int[][]myArray = new int[rows][];
+
+      for (int i = 0; i < rows; i++) {
+        myArray[i] = new int[(int)(Math.random()* cols)];
+        for (int j = 0; j < myArray[i].length; j++) {
+          myArray[i][j] = (int)(Math.random()* maxValue);
+        }
+      }
+      return myArray;
+    }
+
 }
